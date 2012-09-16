078b63a32562274f9cd7ba37ec29d8fa0faf0631
Simon Speich <simon.speich@gmail.com>
2012-09-16 15:04:01 +0200

    added method to read accept-language header

--- scripts/php/Website.php
+++ scripts/php/Website.php
@@ -279,11 +279,48 @@ class Website	{
 	}
 
 	/**
-   * Returns the language code.
-   * @see Website::$lang
-   */
+	 * Returns the language code.
+	 * @see Website::$lang
+	 */
 	public function getLang() {
 		return self::$lang;
+	}
+
+	/**
+	 * Returns an array containing the content from the accept-language header.
+	 * e.g. Array (
+	 *    [en-ca] => 1
+	 *    [en] => 0.8
+	 *    [en-us] => 0.6
+	 *    [de-de] => 0.4
+	 *    [de] => 0.2
+	 * )
+	 * @return array
+	 */
+	public function getLangHeader() {
+		$langs = array();
+
+		// @see http://www.thefutureoftheweb.com/blog/use-accept-language-header
+		if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+			// break up string into pieces (languages and q factors)
+			preg_match_all('/([a-z]{1,8}(-[a-z]{1,8})?)\s*(;\s*q\s*=\s*(1|0\.[0-9]+))?/i', $_SERVER['HTTP_ACCEPT_LANGUAGE'], $arrLang);
+
+			if (count($arrLang[1]) > 0) {
+				// create a list like "en" => 0.8
+				$langs = array_combine($arrLang[1], $arrLang[4]);
+
+				// set default to 1 for any without q factor
+				foreach ($langs as $lang => $val) {
+					if ($val === '') {
+						$langs[$lang] = 1;
+					}
+				}
+
+				// sort list based on value
+				arsort($langs, SORT_NUMERIC);
+			}
+		}
+		return $langs;
 	}
 
 	/**
