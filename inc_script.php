ebdbf84d2f707fdb10c10f225fc7352e3ead6f59
Simon Speich <simon.speich@gmail.com>
2014-01-22 22:22:57 +0100

    use magic constant DIR instead of DOCUMENT_ROOT

--- inc_script.php
+++ inc_script.php
@@ -2,11 +2,9 @@
 use WebsiteTemplate\Language;
 
 session_start();
-//set_time_limit(300);
 if (!isset($_SESSION['loggedIn'])) { $_SESSION['loggedIn'] = false; }
 
-// make include paths available to pages independent on subdir they reside in
-$path = rtrim($_SERVER['DOCUMENT_ROOT'], DIRECTORY_SEPARATOR).'/';	// may have trailing slash depending of environment
+$path = __DIR__.'/';
 $incPath = $path.'scripts/php'.PATH_SEPARATOR;
 $incPath.= $path.'layout'.PATH_SEPARATOR;
 $incPath.= $path.'library';
@@ -17,7 +15,7 @@ require_once 'Language.php';
 require_once 'Menu.php';
 
 $web = new Language();
-$web->lastUpdate = '15.12.2013';
+$web->lastUpdate = '22.01.2014';
 $web->pageTitle = 'Website default';
 
 require_once 'inc_nav.php';
