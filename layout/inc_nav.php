0972d59970b04b94deb6115c7789e941b2f15c3a
Simon Speich <simon.speich@gmail.com>
2014-01-23 10:35:25 +0100

    added some menu items to add separators with css

--- layout/inc_nav.php
+++ layout/inc_nav.php
@@ -20,12 +20,15 @@ $menu1->setActive($web->getWebRoot().'services.php');
 $menu2 = new Menu('menu2', 'menu menu2', array(
 	array(1, 0, 'Home', $web->getWebRoot().'index.php'),
 	array(2, 0, 'About'),
-		array(8, 2, 'Me'),
+		array(8, 2, 'Me...'),
 			array(9, 8, 'You', $web->getWebRoot().'about.php'),
+		array(13, 2, 'CV', $web->getWebRoot().'cv.php'),
 	array(3, 0, 'Services'),
 		array(4, 3, 'more', $web->getWebRoot().'services/more.php'),
-		array(5, 3, 'any'),
+		array(5, 3, 'any...'),
 			array(10, 5, 'subany', $web->getWebRoot().'services/load.php'),
+			array(11, 5, 'subsome', $web->getWebRoot().'services/load.php'),
+			array(12, 5, 'submore', $web->getWebRoot().'services/load.php'),
 		array(6, 3, 'some', $web->getWebRoot().'servcies/save.php'),
 	array(7, 0, 'Contact', $web->getWebRoot().'contact.php')
 ));
