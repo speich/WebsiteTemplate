60d51c6a06521241d61b8682aa7b330b58cc2d18
Simon Speich <simon.speich@gmail.com>
2014-03-18 13:39:55 +0100

    - simplified method getQuery() - added getter for protocol property

--- scripts/php/Website.php
+++ scripts/php/Website.php
@@ -13,21 +13,24 @@ class Website {
 	/** @var string host e.g. www.lfi.ch */
 	public $host;
 
+	/** @var string */
+	public $protocol = 'http';
+
 	/** @var string current path from root including page, e.g. /library/inc_global.php */
 	public $path;
-	
+
 	/** @var string current page without path, e.g. inc_global.php */
 	public $page;
-	
+
 	/** @var string current path without page, e.g. /library */
 	private $dir;
-	
+
 	/** @var string query string without leading question mark */
 	public $query = '';
-	
+
 	/** @var string default page title */
 	public $pageTitle = '';
-	
+
 	/** @var string namespace for session variables */
 	public $namespace = 'web';
 
@@ -36,9 +39,12 @@ class Website {
 
 	/** @var string character set */
 	public $charset = 'utf-8';
-	
+
 	/** @var string date of last update */
 	public $lastUpdate = '';
+
+	/** @var string name of index page */
+	public $indexPage = 'index.php';
 
 	/**
 	 * Creates a new instance of the class Web.
@@ -147,90 +153,102 @@ class Website {
 		else {
 			$this->query = http_build_query($arrQuery);
 		}
+
 		return '?'.htmlspecialchars($this->query);
 	}
 
 	/**
-		 * Returns the current query string.
-		 *
-		 * You can optionally add or remove key-value pairs from the returned querystring without changing it,
-		 * e.g. same as AddQuery or DelQuery but Query property remains unchanged.
-		 * If second argument is an array, the first array is used to add, the second to delete. Otherwise
-		 * second argument is:
-		 * 1 = add (default), 2 = remove
-		 * @param array $arrQuery
-		 * @param integer|array $modifier
-		 * @return string query string
-		 */
-		public function getQuery($arrQuery = null, $modifier = 1) {
-			if (is_null($arrQuery)) {
-				if ($this->query != '') {
-					return '?'.htmlspecialchars($this->query);
-				}
-				else {
-					return '';
-				}
+	 * Returns the current query string.
+	 *
+	 * Returns the query string including the question mark or an empty string
+	 * You can optionally add or remove key-value pairs from the returned querystring without changing it,
+	 * e.g. same as addQuery or delQuery but the query property remains unchanged.
+	 * If second argument is an array, the first array is used to add, the second to delete. Otherwise
+	 * second argument is:
+	 * 1 = add (default), 2 = remove
+	 * @param array $arrQuery
+	 * @param integer|array $modifier
+	 * @return string query string
+	 */
+	public function getQuery($arrQuery = null, $modifier = 1) {
+		if (is_null($arrQuery)) {
+			if ($this->query === '') {
+				return '';
 			}
 			else {
-				if (is_array($modifier)) { // all of second array is to delete
-					$str = $this->getQuery($modifier, 2);
-					$str = str_replace('?', '', $str);
-					$str = html_entity_decode($str);
-					parse_str($str, $arrVar);
-					$arrQuery = array_merge($arrVar, $arrQuery);
-					return '?'.htmlspecialchars(http_build_query($arrQuery));
-				}
-				else { // first array is either add or delete, no second array
-					if ($this->query != '') { // check if query var(s) already exists -> overwrite with new or append
-						parse_str($this->query, $arrVar);
-						if ($modifier == 1) {
-							$arrQuery = array_merge($arrVar, $arrQuery); // if arrays have same string keys, the later key will overwrite the previous
-							return '?'.htmlspecialchars(http_build_query($arrQuery)); // update local $this->Query
-						}
-						else {
-							if ($modifier == 2) {
-								$arr = array(); // make array keys for array_diff_key
-								foreach ($arrQuery as $QueryVar) {
-									$arr[$QueryVar] = null;
-								}
-								$arrQuery = array_diff_key($arrVar, $arr);
-								if (count($arrQuery) > 0) {
-									return '?'.htmlspecialchars(http_build_query($arrQuery));
-								}
-								else {
-									return '';
-								}
+				parse_str($this->query, $arrQuery);
+			}
+		}
+		else {
+			if (is_array($modifier)) { // all of second array is to delete
+				$str = $this->getQuery($modifier, 2);
+				$str = str_replace('?', '', $str);
+				$str = html_entity_decode($str);
+				parse_str($str, $arrVar);
+				$arrQuery = array_merge($arrVar, $arrQuery);
+			}
+			else { // first array is either add or delete, no second array
+				if ($this->query != '') { // check if query var(s) already exists -> overwrite with new or append
+					parse_str($this->query, $arrVar);
+					if ($modifier == 1) {
+						$arrQuery = array_merge($arrVar, $arrQuery); // if arrays have same string keys, the later key will overwrite the previous
+					}
+					else {
+						if ($modifier == 2) {
+							$arr = array(); // make array keys for array_diff_key
+							foreach ($arrQuery as $QueryVar) {
+								$arr[$QueryVar] = null;
+							}
+							$arrQuery = array_diff_key($arrVar, $arr);
+							if (count($arrQuery) === 0) {
+								return '';
 							}
 						}
 					}
-					else {
-						if ($modifier == 1) {
-							return '?'.htmlspecialchars(http_build_query($arrQuery));
-						}
-						else {
-							return '';
-						}
+				}
+				else {
+					if ($modifier !== 1) {
+						return '';
 					}
 				}
 			}
 		}
 
-		/**
-		 * Removes key-value pairs from querystring before returning it.
-		 * @return array
-		 * @param array|string $arrQuery Object
-		 */
-		public function delQuery($arrQuery) {
-			if (!is_array($arrQuery)) {
-				$arrQuery = array($arrQuery);
-			}
-			if ($this->query != '') {
-				foreach ($arrQuery as $queryVar) {
-					$pattern = '/&?'.$queryVar.'=[^\&]*/';
-					$this->query = preg_replace($pattern, '', $this->query);
-				}
-			}
-			$this->query = preg_replace('/^\&/', '', $this->query); // if first key-value pair was removed change ampersand to questions mark
-			return htmlspecialchars($this->getQuery());
+		return '?'.htmlspecialchars(http_build_query($arrQuery, '', '&amp;'));
+	}
+
+	/**
+	 * Removes key-value pairs from querystring before returning it.
+	 * @return array
+	 * @param array|string $arrQuery Object
+	 */
+	public function delQuery($arrQuery) {
+		if (!is_array($arrQuery)) {
+			$arrQuery = array($arrQuery);
 		}
+		if ($this->query != '') {
+			foreach ($arrQuery as $queryVar) {
+				$pattern = '/&?'.$queryVar.'=[^\&]*/';
+				$this->query = preg_replace($pattern, '', $this->query);
+			}
+		}
+		$this->query = preg_replace('/^\&/', '', $this->query); // if first key-value pair was removed change ampersand to questions mark
+		return htmlspecialchars($this->getQuery());
+	}
+
+	/**
+	 * Returns the current protocol.
+	 * Returns the current protocol including the colon and the double slashes e.g. <protocol>://
+	 * unless false is passed as the method argument.
+	 * @param bool $full return additional characters ?
+	 * @return string
+	 */
+	public function getProtocol($full = true) {
+		if ($full) {
+			return $this->protocol.'://';
+		}
+		else {
+			return $this->protocol;
+			}
+	}
 }
