6722a31471ac2a390848ea04af530f52fb8d874d
Simon Speich <simon.speich@gmail.com>
2015-04-23 11:31:02 +0200

    -totally silly use of inheritance (shame on me) -split into two files containing each onyl one class

--- scripts/php/Menu.php
+++ scripts/php/Menu.php
@@ -16,117 +16,12 @@ namespace WebsiteTemplate;
  * 	A menu can be open without being active, when $allChildrenToBeRendered is set to true.
  */
 
-
-/**
- * Class to create menu items.
- */
-class MenuItem {
-
-	/**  @var null|string id */
-	public $id = null;
-
-	/** @var null|string parent id */
-	public $parentId = null;
-
-	/** @var string text of link */
-	public $linkTxt = '';
-
-	/** @var string url of link  */
-	public $linkUrl = '';
-
-	/** @var null|string CSS class name */
-	private $cssClass = null;
-	
-	/** @var bool render children */
-	private $childToBeRendered = false;
-
-	/** @var bool is item active */
-	private $active = false;
-
-	/** @var bool has item an active child */
-	private $hasActiveChild = false;
-
-	/**
-	 * Constructs the menu item.
-	 * @param integer|string $id unique id
-	 * @param integer|string $parentId id of parent item
-	 * @param string $linkTxt link text
-	 * @param string [$linkUrl] link url
-	 */
-	public function __construct($id, $parentId, $linkTxt, $linkUrl = null) {
-		$this->id = $id;
-		$this->parentId = $parentId;
-		$this->linkTxt = $linkTxt;
-		$this->linkUrl = $linkUrl;
-	}
-	
-	/** Get item property if children will be rendered */
-	public function getChildToBeRendered() { return $this->childToBeRendered; }
-	
-	/**
-	 * Set item property if children will be rendered.
-	 * @param bool [$ChildToBeRendered]
-	 */
-	public function setChildToBeRendered($childrenToBeRendered = true) {
-		$this->childToBeRendered = $childrenToBeRendered;
-	}
-	
-	/**
-	 * Set item to be active.
-	 * @param bool [$active]
-	 */
-	public function setActive($active = true) {
-		$this->active = $active;
-	}
-	
-	/**
-	 * Get item active status.
-	 * @return bool
-	 */
-	public function getActive() {
-		return $this->active;
-	}
-
-	/**
-	 * @param boolean $hasActiveChild
-	 */
-	public function setHasActiveChild($hasActiveChild) {
-		$this->hasActiveChild = $hasActiveChild;
-	}
-
-	/**
-	 * @return boolean
-	 */
-	public function getHasActiveChild() {
-		return $this->hasActiveChild;
-	}
-	
-	/**
-	 * Adds a css class to the item.
-	 * Allows to have mutliple CSS classes per item.
-	 * @param string $name CSS class name
-	 */
-	public function addCssClass($name) {
-		if (!is_null($this->cssClass)) {
-			$this->cssClass.= ' ';	// multiple classes have to be separated by a space 
-		}
-		$this->cssClass.= $name;
-	}
-	
-	/**
-	 * Returns the css class string
-	 * @return string 
-	 */
-	public function getCssClass() {
-		return $this->cssClass;
-	}
-}
-
+require_once 'MenuItem.php';
 
 /**
  * Creates the menu.
  */
-class Menu extends MenuItem {
+class Menu {
 	/**
 	 * Holds array of menu items.
 	 * @var MenuItem[] menu items
@@ -179,7 +74,6 @@ class Menu extends MenuItem {
 	/** @var string CSS class name, when item hast at least one active child */
 	public $cssItemActiveChild = 'menuHasActiveChild';
 
-	
 	/**
 	 * Constructs the menu.
 	 * You can provide a 2-dim array with all menu items 
---
+++ scripts/php/MenuItem.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * This file contains two classes to create a navigation menu.
+ * @author Simon Speich
+ */
+namespace WebsiteTemplate;
+
+/**
+ * Class to create menu items.
+ * MenuItems are part of a simple recursive php menu.
+ * @see Menu
+ */
+class MenuItem {
+
+	/**  @var null|string id */
+	public $id = null;
+
+	/** @var null|string parent id */
+	public $parentId = null;
+
+	/** @var string text of link */
+	public $linkTxt = '';
+
+	/** @var string url of link  */
+	public $linkUrl = '';
+
+	/** @var null|string CSS class name */
+	private $cssClass = null;
+	
+	/** @var bool render children */
+	private $childToBeRendered = false;
+
+	/** @var bool is item active */
+	private $active = false;
+
+	/** @var bool has item an active child */
+	private $hasActiveChild = false;
+
+	/**
+	 * Constructs the menu item.
+	 * @param integer|string $id unique id
+	 * @param integer|string $parentId id of parent item
+	 * @param string $linkTxt link text
+	 * @param string [$linkUrl] link url
+	 */
+	public function __construct($id, $parentId, $linkTxt, $linkUrl = null) {
+		$this->id = $id;
+		$this->parentId = $parentId;
+		$this->linkTxt = $linkTxt;
+		$this->linkUrl = $linkUrl;
+	}
+	
+	/** Get item property if children will be rendered */
+	public function getChildToBeRendered() { return $this->childToBeRendered; }
+	
+	/**
+	 * Set item property if children will be rendered.
+	 * @param bool [$ChildToBeRendered]
+	 */
+	public function setChildToBeRendered($childrenToBeRendered = true) {
+		$this->childToBeRendered = $childrenToBeRendered;
+	}
+	
+	/**
+	 * Set item to be active.
+	 * @param bool [$active]
+	 */
+	public function setActive($active = true) {
+		$this->active = $active;
+	}
+	
+	/**
+	 * Get item active status.
+	 * @return bool
+	 */
+	public function getActive() {
+		return $this->active;
+	}
+
+	/**
+	 * @param boolean $hasActiveChild
+	 */
+	public function setHasActiveChild($hasActiveChild) {
+		$this->hasActiveChild = $hasActiveChild;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function getHasActiveChild() {
+		return $this->hasActiveChild;
+	}
+	
+	/**
+	 * Adds a css class to the item.
+	 * Allows to have mutliple CSS classes per item.
+	 * @param string $name CSS class name
+	 */
+	public function addCssClass($name) {
+		if (!is_null($this->cssClass)) {
+			$this->cssClass.= ' ';	// multiple classes have to be separated by a space 
+		}
+		$this->cssClass.= $name;
+	}
+	
+	/**
+	 * Returns the css class string
+	 * @return string 
+	 */
+	public function getCssClass() {
+		return $this->cssClass;
+	}
+}
