6e3436e6debe6bfee0b6d626189651692a53d018
Simon <simon.speich@gmail.com>
2018-06-30 12:40:44 +0200

    -fixes missing parent call -use Website property from constructor instead of passing it to method

--- scripts/php/LanguageMenu.php
+++ scripts/php/LanguageMenu.php
@@ -2,6 +2,10 @@
 
 namespace WebsiteTemplate;
 
+/**
+ * Class LanguageMenu
+ * Class to render a menu to change the language
+ */
 class LanguageMenu extends Language
 {
     /** @var string id attribute of HTMLUListElement */
@@ -19,32 +23,37 @@ class LanguageMenu extends Language
 	/** @var string url to redirect to if page does not exists in that language */
 	public $redirect;
 
+	/** @var Website */
+    private $web;
+
     /**
      * LanguageMenu constructor.
      * @param Website $web
      */
 	public function __construct(Website $web)
     {
+        parent::__construct();
+        $this->web = $web;
         $this->redirect = '/'.$web->indexPage;
     }
 
     /**
      * Returns a HTML string with links to the current page in all available languages.
-     * Method checks if the page exist for each language. If it doesn't, it will direct to the language switcher page
-     * referenced with the property LanguageMenu::redirect
+     * Method checks if the page exist for each language. If it doesn't, the link will point to a language switcher page,
+     * which is referenced with the property LanguageMenu::redirect
      * @param Website $web
      * @return string html
      */
-    public function render($web)
+    public function render()
     {
         $query = new QueryString();
         $count = 0;
         $str = '';
         $str .= '<ul id="'.$this->ulId.'" class="'.$this->ulClass.'">';
         foreach ($this->arrLang as $lang => $label) {
-            $page = $this->createPage($web->page, $lang);
-            $path = $web->getDir();
-            if (file_exists($web->getDocRoot().$path.$page)) {
+            $page = $this->createPage($this->web->page, $lang);
+            $path = $this->web->getDir();
+            if (file_exists($this->web->getDocRoot().$path.$page)) {
                 $url = $path.$page.$query->withString(array('lang' => $lang));
             } else {
                 $url = $this->redirect.$query->withString(array('lang' => $lang, 'url' => $path.$page));
