58342cebab5d9f6ea32fa67af88865aaa3042a02
Simon Speich <simon.speich@wsl.ch>
2022-07-27 16:48:25 +0200

    - rely on composer autoloading instead of setting include path

--- inc_script.php
+++ inc_script.php
@@ -6,11 +6,7 @@ use WebsiteTemplate\Website;
 require_once __DIR__.'/vendor/autoload.php';
 
 
-$path = __DIR__ . '/';
-$incPath = $path . 'layout' . PATH_SEPARATOR;
-set_include_path($incPath);
 date_default_timezone_set('Europe/Zurich');
-
 
 $language = new Language();
 $language->autoDetect();
@@ -20,4 +16,4 @@ $web = new Website($domains);
 $web->setLastUpdate('03.08.2020');
 $web->pageTitle = 'Website Template';
 
-require_once __DIR__ . '/layout/inc_nav.php';
+require_once __DIR__.'/layout/inc_nav.php';
