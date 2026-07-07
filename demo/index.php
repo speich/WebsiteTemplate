09bd09e79ceb0f5e7fab4a05c0c9a26b8dc00bf0
Simon <info@speich.net>
2026-07-07 10:17:13 +0200

    remove unused div

--- demo/index.php
+++ demo/index.php
@@ -2,23 +2,21 @@
 <!DOCTYPE html>
 <html lang="<?php echo $language->get(); ?>" dir="ltr">
 <head>
-<title><?php echo $web->pageTitle; ?></title>
-<?php echo $head->render(); ?>
+    <title><?php echo $web->pageTitle; ?></title>
+    <?php echo $head->render(); ?>
 </head>
 
 <body>
-<p>Default Website Project Template</p>
+<h1>Default Website Project Template</h1>
 <h2>Cascading dropdown menu</h2>
 <p>An animated and fluid menu in pure CSS with nested sub menus.</p>
 <?php echo $menu2->render(); ?>
 
 <h2>PHP menu</h2>
-<div id="layoutMenu3">
-    <?php echo $menu3->render(); ?>
-</div>
+<?php echo $menu3->render(); ?>
+
 <h2>PHP Menu all open</h2>
-<div id="layoutMenu4">
-    <?php echo $menu4->render(); ?></div>
-<div id="layoutContent">
+<?php echo $menu4->render(); ?>
+
 </body>
 </html>
